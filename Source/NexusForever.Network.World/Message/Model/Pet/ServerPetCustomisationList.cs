592aabd89a45479ba968db98fd17973259d1f7c9
krakal <krakaldev@gmail.com>
2025-08-07 10:19:12 +0100

    Pet messages (#516)
    * * Initial
    
    * * Adds .Pet namespace for pet related things, including combat pets, vanity pets, and scanbots
    * Adds a few missing pet related messages
    
    * Renamed ServerClearPetFlair to be more consistent with the other pet related packets.
    
    * Updated spelling to be consistent.
    
    ---------
    
    Co-authored-by: Rawaho <rawahodev@gmail.com>

--- Source/NexusForever.Network.World/Message/Model/Pet/ServerPetCustomisationList.cs
+++ Source/NexusForever.Network.World/Message/Model/Pet/ServerPetCustomisationList.cs
@@ -1,9 +1,9 @@
 ﻿using NexusForever.Network.Message;
 using NexusForever.Network.World.Message.Model.Shared;
 
-namespace NexusForever.Network.World.Message.Model
+namespace NexusForever.Network.World.Message.Model.Pet
 {
-    [Message(GameMessageOpcode.ServerPetCustomizationList)]
+    [Message(GameMessageOpcode.ServerPetCustomisationList)]
     public class ServerPetCustomisationList : IWritable
     {
         public NetworkBitArray UnlockedFlair { get; set; } = new(512, NetworkBitArray.BitOrder.MostSignificantBit);
@@ -14,8 +14,8 @@ namespace NexusForever.Network.World.Message.Model
             writer.WriteBytes(UnlockedFlair.GetBuffer());
 
             writer.Write(PetCustomisations.Count, 32u);
-            foreach(var petCustomization in PetCustomisations)
-                petCustomization.Write(writer);
+            foreach (var petCustomisation in PetCustomisations)
+                petCustomisation.Write(writer);
         }
     }
 }
